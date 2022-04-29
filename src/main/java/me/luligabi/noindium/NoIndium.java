d084055b18ef1a6044ac1e16ee898ff31332f96f
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2022-04-29 16:22:26 -0300

    Use event's MinecraftClient instance instead of getting one manually.

--- src/main/java/me/luligabi/noindium/NoIndium.java
+++ src/main/java/me/luligabi/noindium/NoIndium.java
@@ -6,7 +6,6 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 
 @Environment(EnvType.CLIENT)
 public class NoIndium implements ClientModInitializer {
@@ -15,7 +14,7 @@ public class NoIndium implements ClientModInitializer {
     public void onInitializeClient() {
         ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
             if((HAS_SODIUM && CONFIG.showIndiumScreen) || (HAS_OPTIFABRIC && CONFIG.showOptifabricScreen)) {
-                MinecraftClient.getInstance().setScreen(new NoIndiumWarningScreen());
+                client.setScreen(new NoIndiumWarningScreen());
             }
         });
     }
